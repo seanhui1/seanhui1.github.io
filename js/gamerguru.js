4e06c9161b4109c5c1c8237ff1cff7bdcb3b74d9
seanhui1 <seanhui1@illinois.edu>
2014-12-08 10:07:45 -0600

    cal-heatmap messing around
    adding features and such

--- js/gamerguru.js
+++ js/gamerguru.js
@@ -1,4 +1,15 @@
-var cal = new CalHeatMap();
-	cal.init({
-		data: "data/datas.json"
-	});
+var calendar = new CalHeatMap();
+calendar
+	data: "datas.json",
+	start: new Date(2014, 1),
+	id : "graph_k",
+	domain : "month",
+	subDomain : "x_day",
+	range : 12,
+	cellsize: 15,
+	cellpadding: 3,
+	cellradius: 5,
+	domainGutter: 15,
+	weekStartOnMonday: 0,
+	scale: [10, 20, 80, 100]
+});
