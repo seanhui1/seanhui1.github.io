0a8bf49ad112c0a67a8c00ee2afa51ab293e19a6
seanhui1 <seanhui1@illinois.edu>
2014-12-08 10:23:25 -0600

    fixed data src
    fixed data src

--- js/gamerguru.js
+++ js/gamerguru.js
@@ -1,6 +1,6 @@
 var calendar = new CalHeatMap();
 calendar.init({
-       data: "datas.json",
+    data: "data/datas.json",
 	start: new Date(2014, 0),
 	id : "graph_k",
 	domain : "month",
@@ -11,5 +11,8 @@ calendar.init({
 	cellradius: 5,
 	domainGutter: 15,
 	weekStartOnMonday: 0,
-	scale: [10, 20, 30, 40]
+	scale: [10, 20, 30, 40],
+	onClick: function(date, count) {
+		alert("potential game URL here");
+	}
     });
