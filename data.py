5717fb6087b6d65023824296552105db88eaf546
seanhui1 <seanhui1@illinois.edu>
2014-12-08 09:37:09 -0600

    fixed game data
    http get url was wrong, fixed

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ offset = 0
 f = open('data/games.json','a')
 while offset<3495:
 	
-	url = "https://www.giantbomb.com/api/releases/?api_key=7e672687f183619ce64402ae075d3c63173c5c5e&format=json&offset="+str(offset)+"field_list=name,release_date,genre,platform&filter=release_date:2014-01-01%2000:00:00|2014-12-31%2000:00:00"
+	url = "https://www.giantbomb.com/api/releases/?api_key=7e672687f183619ce64402ae075d3c63173c5c5e&format=json&offset="+str(offset)+"&field_list=name,release_date,genre,platform&filter=release_date:2014-01-01%2000:00:00|2014-12-31%2000:00:00"
 	response = urllib.urlopen(url)
 	data = json.loads(response.read())
 	json.dump(data,f)
