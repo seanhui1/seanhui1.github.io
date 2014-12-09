12475080f425407696924fb72580b5bd44ae26a7
seanhui1 <seanhui1@illinois.edu>
2014-12-09 13:56:43 -0600

    updated description
    updated description, labellegend text, backgrund color for heat cal

--- js/gamerguru.js
+++ js/gamerguru.js
@@ -6,6 +6,11 @@ calendar.init({
 	domain : "month",
 	subDomain : "x_day",
 	legend: [60, 70, 80, 90],
+	legendLabel: {
+			lower: "Releases that scored within {min} {name}",
+			inner: "Releases that scored within et {up} {name}",
+			upper: "Releases that scored within{max} {name}"
+		},
 	range : 12,
 	cellsize: 15,
 	cellpadding: 3,
