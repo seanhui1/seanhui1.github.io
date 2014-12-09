2099dbc1bc09027d71f72430c34ade480400dd56
seanhui1 <seanhui1@illinois.edu>
2014-12-09 00:07:40 -0600

    a

--- js/gamerguru.js
+++ js/gamerguru.js
@@ -5,11 +5,12 @@ calendar.init({
 	id : "graph_k",
 	domain : "month",
 	subDomain : "x_day",
+	scale: [60, 80, 90, 100]
 	range : 12,
 	cellsize: 15,
 	cellpadding: 3,
 	cellradius: 5,
 	domainGutter: 15,
 	weekStartOnMonday: 0,
-	scale: [60, 80, 90, 100]
+	
     });
